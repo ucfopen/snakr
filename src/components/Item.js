c7981eb437874f1cecdf9b8c1e32c6725c57737a
Alyssa <allygator95@gmail.com>
2019-07-30 10:19:24 -0400

    Added balance math

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,18 +1,21 @@
-import React, {useContext, useEffect, useRef} from 'react';
+import React, {useContext, useEffect, useRef, useState} from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
 import {FirebaseContext} from './Firebase';
+import UserContext from './UserContext';
 
 function Item(props) {
     const unformatted = useRef(props.item.data().price);
+    const userData = useContext(UserContext);
     const price = useRef(props.item.data().price);
     const name = useRef(props.item.data().name);
     const amount = useRef(props.item.data().count);
     let cost = "$"+(price.current).toFixed(2);
     const firebase = useContext(FirebaseContext);
+    const [dbUser, updatedb] = useState({});
 
     useEffect(() => {
         firebase.db.collection('items').doc(props.item.id).onSnapshot(function(doc) {
@@ -25,6 +28,16 @@ function Item(props) {
         })
     }, [firebase]);
 
+    useEffect(() => {
+        firebase.db.collection('users').doc(userData.authUser.uid).onSnapshot(function(doc) {
+            updatedb(doc.data());
+        })
+    }, [firebase]);
+
+    function updateBank(amount) {
+        firebase.db.collection('users').doc(userData.authUser.uid).set({bank:dbUser.bank-amount, admin: dbUser.admin})
+    }
+
     function buyItem() {
         firebase.db.collection('items').doc(props.item.id).set({count: amount.current-1, name: name.current, price: unformatted.current}).then(function() {
         console.log("Document successfully written!");
@@ -32,6 +45,7 @@ function Item(props) {
         .catch(function(error) {
             console.error("Error writing document: ", error);
         });
+        updateBank(unformatted.current);
     }
 
     return (
