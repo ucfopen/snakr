dc7c08c6d28825a4fe59bd114debde7ccfe1c297
Alyssa Davis <alyssa.davis@ucf.edu>
2019-08-01 13:08:08 -0400

    Devin wanted an apostrophe

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -12,7 +12,7 @@ function Welcome() {
         <h2>Concerned about what's going on in Zach's square?</h2>
         <h2>Snakr to the rescue</h2>
         <h3>Snakr means an always up to date list of what's in stock for snacks</h3>
-        <h3>Snakr means you'll always know how much you owe (or dont owe!)</h3>
+        <h3>Snakr means you'll always know how much you owe (or don't owe!)</h3>
         <h4>Login to get started with the Snakr revolution.</h4>
         <Link to="/login"><Button variant="contained">Login</Button></Link>
       </div>
